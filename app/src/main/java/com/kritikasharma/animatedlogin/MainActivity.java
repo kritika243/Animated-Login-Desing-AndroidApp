c31cc4d9db367e2e0586cdda0308944f10ab88ea
kritika243 <24kritikasharma24@gmail.com>
2021-05-08 17:15:16 +0530

    added animation to background

--- app/src/main/java/com/kritikasharma/animatedlogin/MainActivity.java
+++ app/src/main/java/com/kritikasharma/animatedlogin/MainActivity.java
@@ -1,14 +1,26 @@
 package com.kritikasharma.animatedlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.layout);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) layout.getBackground();
+
+        animationDrawable.setEnterFadeDuration(750);
+        animationDrawable.setExitFadeDuration(750);
+        animationDrawable.start();
     }
 }
